cfeb1507b9fd4888e9c655cbe7114c6c89935a72
Xiang Liu <liuxiang@0587375143.wireless.umich.net>
2017-04-05 12:04:38 -0400

    fixed directory slash

--- loadModules.m
+++ loadModules.m
@@ -2,11 +2,11 @@ function [  ] = loadModules(  )
 %LOADMODULES Summary of this function goes here
 %   Detailed explanation goes here
 
-    addpath([pwd '\Data']);
-    addpath([pwd '\kfdatatools']);
-    addpath([pwd '\kfregression']);
-    addpath([pwd '\kftntcore']);
-    addpath([pwd '\kfpareto']);
+    addpath([pwd '/Data']);
+    addpath([pwd '/kfdatatools']);
+    addpath([pwd '/kfregression']);
+    addpath([pwd '/kftntcore']);
+    addpath([pwd '/kfpareto']);
 
 end
 
